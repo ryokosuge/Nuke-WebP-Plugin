ab00ede59bec5c181099fd11a49e8891e2d34ae7
ryokosuge <ryo.kosuge@gmail.com>
2018-01-17 12:26:04 +0900

    Implement processing to convert webp to Image.

---
+++ Source/WebP/WebPDataDecoder.h
@@ -0,0 +1,21 @@
+//
+//  WebPDataDecoder.h
+//  Nuke-WebP-Plugin iOS
+//
+//  Created by nagisa-kosuge on 2018/01/17.
+//  Copyright © 2018年 RyoKosuge. All rights reserved.
+//
+
+#import "WebPImageMacros.h"
+
+#if WEBP_IMAGE_MAC
+#import <AppKit/AppKit.h>
+#define Image   NSImage
+#else
+#import <UIKit/UIKit.h>
+#define Image   UIImage
+#endif
+
+@interface WebPDataDecoder : NSObject
++ (nullable Image *)decodeData:(nonnull NSData *)data;
+@end
