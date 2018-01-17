6253197d1ff1b40347edd41c4c3bbf075436db33
ryokosuge <ryo.kosuge@gmail.com>
2018-01-17 18:24:27 +0900

    Create Demo project.

---
+++ Demo/Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by nagisa-kosuge on 2018/01/17.
+//  Copyright © 2018年 RyoKosuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
